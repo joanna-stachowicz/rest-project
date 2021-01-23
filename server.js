b2fa2185bf73393b15312324f3212f790401b2f2
Joanna Stachowicz <joannastachowicz19@gmail.com>
2021-01-23 21:16:12 +0100

    Add the client folder

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
@@ -9,7 +10,7 @@ app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
 
-// app.use(cors());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', testimonialsRoutes);
